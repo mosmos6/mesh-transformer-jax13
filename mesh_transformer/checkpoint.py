74b1f0710b11798a396117622e9da7c20b236b56
mosmos6 <93764745+mosmos6@users.noreply.github.com>
2025-04-11 18:22:07 +0300

    Update checkpoint.py

--- mesh_transformer/checkpoint.py
+++ mesh_transformer/checkpoint.py
@@ -138,7 +138,12 @@ def read_ckpt(pytree, dir, shards_in, shards_out=None, load_opt=True):
     old_flattened, structure = jax.tree_util.tree_flatten(pytree)
 
     #original_opt_state = pytree["opt_state"]
-    original_opt_state = pytree.get("opt_state", {})
+    
+    if load_opt and "opt_state" in pytree: 
+        original_opt_state = pytree["opt_state"]
+    else:
+        original_opt_state = {}
+
 
 
     # TODO: figure out how to use a process pool here for more speed
